bb1b6adec65f5036af1868f2f23735a9a5ee7476
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-10-29 12:01:58 +0300

    still working on linked lists

--- DSA/singly_linkedlist.c
+++ DSA/singly_linkedlist.c
@@ -22,7 +22,50 @@ struct PacketDescriptor
 };
 
 
-//a node in the sinly linked list
+//a node in the singly linked list
+
+struct PacketNode
+{
+	struct PacketDescriptor packet; //the packet data
+	struct PacketNode *next;
+};
+
+//the linked list itself
+struct PacketList 
+{
+	struct PacketNode *head; //first node 
+};
+
+
+//initialise an empty list
+
+void initialize_list(struct PacketList* list)
+{
+	list->head=NULL;
+}
+//
+//
+//add packet to the front(insert at beginning)
+//this is a very common operation in networking- new packet arrive, add it to the list 
+//
+void add_packet_to_front(struct PackeList * list, struct PacketDescripror packet)
+{
+	//allocate memory for new node
+	struct PacketNode* new_node=malloc(sizeof(struct PacketNode));
+	if(!new_node)
+	{
+		perror("mem allocation failed\n");
+		return;
+	}
+	//fill node
+	new_node->packet=packet;
+	new_node->next = list->head;// new node point to old first node
+				    
+	list->head = new_node;//now new node i sthe first node
+}
+
+//a  new packet comes in and we attch it to the current queue
+
 
 
 
